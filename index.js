ca21cf5583d40e6a3fe888a09f2f71c9292cb3e3
Pallabcaritor <pallab@caritorsolutions.in>
2024-08-28 04:29:55 +0530

    getdata api updated

--- index.js
+++ index.js
@@ -64,7 +64,22 @@ app.get('/getorderwindow', (req, res, next) => {
 
     var constants = require('./queries');
 
-    if (season.season != undefined)
+    if ((season.season != undefined) & (season.order_window_label != undefined))
+	{
+		if (season.season.length != 0)
+			{
+				const windowlabelValue2 = season.order_window_label
+				var wl = windowlabelValue2.split(",");
+                windowlabelValue = "'" + wl.join("','") + "'";
+				const query = (constants.ORDER_WINDOW_QUERY_BY_WL).replace('{windowlabelValue}', windowlabelValue)
+				console.log(`Final query getOrderWindow----> ${query}`)
+				pool.query(query)
+				.then(testData => {
+					res.send(testData.rows);
+				})
+			}
+	}
+	else if (season.season != undefined)
 		{
 			if (season.season.length == 0)
 			{
@@ -338,6 +353,203 @@ app.get('/getfilter2', (req, res, next) => {
 	// res.status(200).send('Ok-Ok');
 	
 })
+
+app.post('/getdata', async(req, res) => {
+    var reqbody = req.body
+    
+	//console.log(`Type of req body----> ${typeof reqbody}`)
+	var constants = require('./queries');
+	var q = constants.BASE_QUERY1
+
+	
+	if(reqbody.season.length != 0)
+	{   
+		seasonValue = reqbody.season
+		q = q.concat(` and o.season_id in ({seasonValue})`.replace('{seasonValue}',seasonValue))
+	}
+
+	if(reqbody.order_window.length != 0)
+	{
+        orderwindowValue = reqbody.order_window
+		q = q.concat(` and o.order_window_id in (select id from t_order_window where season_id in ({seasonValue}) order by 1 desc limit 20)`.replace('{seasonValue}',seasonValue))
+	} 
+
+	if(reqbody.category.length != 0)
+	{
+		categoryValue2 = String(reqbody.category)
+		var catv = categoryValue2.split(",");
+        categoryValue = "'" + catv.join("','") + "'";
+		q = q.concat(` and tfmi.category1 in ({categoryValue})`.replace('{categoryValue}',categoryValue))
+	} 
+
+	if(reqbody.remarks.length != 0)
+	{
+		remarksValue2 = String(reqbody.remarks)
+		var remarksv = remarksValue2.split(",");
+		remarksValue = "'" + remarksv.join("','") + "'";
+		q = q.concat(` and tfmi.remarks in ({remarksValue})`.replace('{remarksValue}',remarksValue))
+	} 
+
+	if(reqbody.og_launch_month.length != 0)
+	{
+		ogValue = String(reqbody.og_launch_month)
+		var ogv = ogValue.split(",");
+		ogMonthValue = "'" + ogv.join("','") + "'";
+		q = q.concat(` and tfmi.og_launch_month in ({ogMonthValue})`.replace('{ogMonthValue}',ogMonthValue))
+	} 
+
+	if(reqbody.item_name.length != 0)
+	{
+		itemNameValue2 = String(reqbody.item_name)
+		var itnv = itemNameValue2.split(",");
+		itemNameValue = "'" + itnv.join("','") + "'";
+		q = q.concat(` and tfmi.item_name in ({itemNameValue})`.replace('{itemNameValue}',itemNameValue))
+	} 
+
+	if(reqbody.item_code.length != 0)
+	{
+		itemCodeValue2 = String(reqbody.item_code)
+		var itcv = itemCodeValue2.split(",");
+		itemCodeValue = "'" + itcv.join("','") + "'";
+		q = q.concat(` and tfmi.item_code in ({itemCodeValue})`.replace('{itemCodeValue}',itemCodeValue))
+	} 
+
+	if(reqbody.regional_distribution_tier.length != 0)
+	{
+		regDis2 = String(reqbody.regional_distribution_tier)
+		var rgdv = regDis2.split(",");
+		regDis = "'" + rgdv.join("','") + "'";
+		q = q.concat(` and tfmi.regional_distribution_tier in ({regDis})`.replace('{regDis}',regDis))
+	} 
+
+	if(reqbody.customer.length != 0)
+	{
+		custval2 = String(reqbody.customer)
+		var custv = custval2.split(",");
+		custval = "'" + custv.join("','") + "'";
+		q = q.concat(` and tf.franchise_name in ({custval})`.replace('{custval}',custval))
+	} 
+
+	if(reqbody.gender.length != 0)
+	{
+		genval2 = String(reqbody.customer)
+		var genv = genval2.split(",");
+		genval = "'" + genv.join("','") + "'";
+		q = q.concat(` and tfmi.gender in ({genval})`.replace('{genval}',genval))
+	} 
+
+	if(reqbody.subsidiary.length != 0)
+	{
+		subval2 = String(reqbody.subsidiary)
+		var subv = subval2.split(",");
+		subval = "'" + subv.join("','") + "'";
+		q = q.concat(` and ms.name in {subval}`.replace('{subval}',subval))
+	} 
+    
+
+	q = q + (constants.GROUP_BY1) + " " + (constants.BASE_QUERY2)
+
+
+	///////////////////////////////////////////////////////////////
+
+	if(reqbody.season.length != 0)
+		{   
+			seasonValue = reqbody.season
+			q = q.concat(` and o.season_id in ({seasonValue})`.replace('{seasonValue}',seasonValue))
+		}
+	
+		if(reqbody.order_window.length != 0)
+		{
+			orderwindowValue = reqbody.order_window
+			q = q.concat(` and o.order_window_id in (select id from t_order_window where season_id in ({seasonValue}) order by 1 desc limit 20)`.replace('{seasonValue}',seasonValue))
+		} 
+	
+		if(reqbody.category.length != 0)
+		{
+			categoryValue2 = String(reqbody.category)
+			var catv = categoryValue2.split(",");
+			categoryValue = "'" + catv.join("','") + "'";
+			q = q.concat(` and tfmi.category1 in ({categoryValue})`.replace('{categoryValue}',categoryValue))
+		} 
+	
+		if(reqbody.remarks.length != 0)
+		{
+			remarksValue2 = String(reqbody.remarks)
+			var remarksv = remarksValue2.split(",");
+			remarksValue = "'" + remarksv.join("','") + "'";
+			q = q.concat(` and tfmi.remarks in ({remarksValue})`.replace('{remarksValue}',remarksValue))
+		} 
+	
+		if(reqbody.og_launch_month.length != 0)
+		{
+			ogValue = String(reqbody.og_launch_month)
+			var ogv = ogValue.split(",");
+			ogMonthValue = "'" + ogv.join("','") + "'";
+			q = q.concat(` and tfmi.og_launch_month in ({ogMonthValue})`.replace('{ogMonthValue}',ogMonthValue))
+		} 
+	
+		if(reqbody.item_name.length != 0)
+		{
+			itemNameValue2 = String(reqbody.item_name)
+			var itnv = itemNameValue2.split(",");
+			itemNameValue = "'" + itnv.join("','") + "'";
+			q = q.concat(` and tfmi.item_name in ({itemNameValue})`.replace('{itemNameValue}',itemNameValue))
+		} 
+	
+		if(reqbody.item_code.length != 0)
+		{
+			itemCodeValue2 = String(reqbody.item_code)
+			var itcv = itemCodeValue2.split(",");
+			itemCodeValue = "'" + itcv.join("','") + "'";
+			q = q.concat(` and tfmi.item_code in ({itemCodeValue})`.replace('{itemCodeValue}',itemCodeValue))
+		} 
+	
+		if(reqbody.regional_distribution_tier.length != 0)
+		{
+			regDis2 = String(reqbody.regional_distribution_tier)
+			var rgdv = regDis2.split(",");
+			regDis = "'" + rgdv.join("','") + "'";
+			q = q.concat(` and tfmi.regional_distribution_tier in ({regDis})`.replace('{regDis}',regDis))
+		} 
+	
+		if(reqbody.customer.length != 0)
+		{
+			custval2 = String(reqbody.customer)
+			var custv = custval2.split(",");
+			custval = "'" + custv.join("','") + "'";
+			q = q.concat(` and tf.franchise_name in ({custval})`.replace('{custval}',custval))
+		} 
+	
+		if(reqbody.gender.length != 0)
+		{
+			genval2 = String(reqbody.customer)
+			var genv = genval2.split(",");
+			genval = "'" + genv.join("','") + "'";
+			q = q.concat(` and tfmi.gender in ({genval})`.replace('{genval}',genval))
+		} 
+	
+		if(reqbody.subsidiary.length != 0)
+		{
+			subval2 = String(reqbody.subsidiary)
+			var subv = subval2.split(",");
+			subval = "'" + subv.join("','") + "'";
+			q = q.concat(` and ms.name in {subval}`.replace('{subval}',subval))
+		} 
+    
+	q =	q + (constants.GROUP_BY2)
+	console.log("----------------------------------------FINAL QUERY--------------------------------------------")
+	console.log(`${q}`)
+	console.log("-----------------------------------------------------------------------------------------------")
+
+	// pool.query(q)
+	// .then(testData => {
+	// 	res.send(testData.rows);
+	// })
+
+	const result = await pool.query(q)
+	// const jsonString = JSON.stringify(result.rows);
+	res.json(result.rows)
+});
 
 // Require the Routes API 
 // Create a Server and run it on the port 3000
